c261710bf2d67adf1eb78d4268e206b80bbe5ce0
Dipankar1990 <dipankarmaity.90@gmail.com>
2020-12-15 20:03:05 -0800

    add comment to profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {isUserloggedin} from '../Helpers';
 import { Redirect } from "react-router-dom";
 import Menu from './Menu'
+/* efwergwegrw*/
 const Profile = () => {
     return ( 
     <>
